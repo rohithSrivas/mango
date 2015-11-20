910a39f7c8b05374502cd9d7d10bd7fd09a2fb06
rsrivas <rsrivas@gmail.com>
2015-11-20 09:32:43 -0800

    Support Script for phantom peak quals
    A wrapper script for running the phantampeakqual ENCODE script.

---
+++ R/runPhantomPeakQual.R
@@ -0,0 +1,33 @@
+#' aligns reads using bowtie
+#'
+#' This function assesses ChIP quality using the phantompeakquals toolkit
+#'  
+#' @param input.bam full path to input bam file
+#' @param output.results.file output file containing ENCODE chip quality metrics and est. fragment length
+#' @param output.results.plot.file output plot containing results of strand cross-correlation
+#' @param path.to.phantom.script path to run_spp.R [script for running phantom peak quals]
+#' @param num.threads Number of parallel threads to utilize for computing strand cross-correlation
+#' @export
+#' 
+run.phantom <- function(	input.bam,
+							output.results.file,
+							output.plot.file,
+							path.to.phantom.script,
+                        	num.threads=1)
+{
+  
+  # setup command
+  qual.command <- paste(	path.to.phantom.script,
+	  						" -c=",input.bam,
+							" -savp=",output.plot.file,
+							" -out=",output.results.file,
+							" -p=",num.threads,sep="")
+  
+  
+  # print command invoked
+  print (qual.command)
+  
+  # execute command
+  system(paste("Rscript ",qual.command,sep=""))
+}
+
